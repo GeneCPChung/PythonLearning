b66abf3d32f3ef32abcad69df072b289cacc35bf
GeneCPChung <genecpc@gmail.com>
2019-04-24 21:36:07 -0400

    loops

---
+++ loops/range.py
@@ -0,0 +1,13 @@
+for num in range(10):
+    print(num)
+
+for num in range (10,20,2):
+    print(num)
+
+for num in range(8,0,-2):
+    print(num)
+
+x = 0
+for add in range(11,20,2):
+    x += add
+    print(x)
