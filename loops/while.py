812f2fbadbeb87a6e3be976e400831abc82bc64e
GeneCPChung <genecpc@gmail.com>
2019-05-20 16:19:55 -0400

    while loops

---
+++ loops/while.py
@@ -0,0 +1,20 @@
+# msg = input("Whats the secret password?")
+# while msg != "bananas":
+#     print("WRONG!")
+#     msg = input("What's the secret password?")
+# print("CORRECT!!")
+
+# for num in range(1, 11):
+#     print(num)
+
+# print("\U0001f600")
+
+num = 1
+while num < 11:
+    print(num)
+    num += 1
+
+
+
+
+
