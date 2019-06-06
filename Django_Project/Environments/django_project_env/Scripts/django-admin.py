f306c42c47738b0a720dbd23b4ba10400d40465c
GeneCPChung <genecpc@gmail.com>
2019-06-05 21:54:28 -0400

    organized files

---
+++ Django_Project/Environments/django_project_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\genec\code\python\pythonlearning\django_project\environments\django_project_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Python_Course/lists/euler_problems.py
@@ -0,0 +1,21 @@
+for pal in range(101, 999):
+    #compare first number and last number
+    #convert and int to a string
+    pal = str(pal)
+
+    print(pal)
+
+# take loop from range of 999 DOWN TO 1 and compare palindrome
+# if palindrome = yes
+#     add to variable
+# else ''
+
+#slice in half and then compare
+
+# loop from range in (100, 1000)
+# for second in range(first, 1000):
+#     product = first * second
+#     product = str(product)
+#     productrev = productstr[::-1]
+#     if productstr = productrev
+#         self.palindrome_list.append(product)
